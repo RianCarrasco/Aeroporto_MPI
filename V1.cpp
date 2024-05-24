28d8fbaa2f3eb95d852bca159fc08a4fc5ed5c25
ViniciusSt1 <stefanesv31@gmail.com>
2024-05-24 20:37:36 -0300

    alterações

--- V1.cpp
+++ V1.cpp
@@ -13,58 +13,103 @@ const int TAG = 0;
 const int REQ_TAG = 1;
 const int ACK_TAG = 2;
 
-struct Voo {
-    int codigo;
+struct Voo
+{
+    string codigo;
     int origem;
-    int destino; // rank destino
-    int horario; // qual tempo lógico ele deve ir
+    int destino;   // rank destino
+    int horario;   // qual tempo lógico ele deve ir
     int tempo_voo; // tempo que a mensagem ficará no ar
     bool is_pouso;
 };
 
-int Lamport(int pi) {
+int Lamport(int pi)
+{
     return (pi * 2) + D;
 }
 
-struct Mensagem {
+struct Mensagem
+{
     int lc; // Relógio lógico
     Voo voo;
 };
 
-void printTabela(const vector<Mensagem>& mensagens, int rank) {
+void printTabela(const vector<Mensagem> &mensagens, int rank)
+{
     cout << "Tabela: Configuração para o aeroporto (" << rank + 1 << ")" << endl;
     cout << "+---------+---------+------------------+---------------+" << endl;
     cout << "| Código  | Origem  | Horário de Chegada | Tempo de Voo |" << endl;
     cout << "| Pousos  |---------|--------------------|--------------|" << endl;
     cout << "+---------+---------+------------------+---------------+" << endl;
-    for (const auto& msg : mensagens) {
-        if (msg.voo.is_pouso) {
+    for (const Mensagem &msg : mensagens)
+    {
+        if (msg.voo.is_pouso)
+        {
             cout << "| " << setw(7) << msg.voo.codigo << " | " << setw(7) << msg.voo.origem << " | "
-                << setw(16) << msg.voo.horario << " | " << setw(13) << msg.voo.tempo_voo << " |" << endl;
+                 << setw(16) << msg.voo.horario << " | " << setw(13) << msg.voo.tempo_voo << " |" << endl;
         }
     }
     cout << "+---------+---------+------------------+---------------+" << endl;
     cout << "| Decola. | Destino | Horário de Partida | Tempo de Voo |" << endl;
     cout << "+---------+---------+------------------+---------------+" << endl;
-    for (const auto& msg : mensagens) {
-        if (!msg.voo.is_pouso) {
+    for (const Mensagem &msg : mensagens)
+    {
+        if (!msg.voo.is_pouso)
+        {
             cout << "| " << setw(7) << msg.voo.codigo << " | " << setw(7) << msg.voo.destino << " | "
-                << setw(16) << msg.voo.horario << " | " << setw(13) << msg.voo.tempo_voo << " |" << endl;
+                 << setw(16) << msg.voo.horario << " | " << setw(13) << msg.voo.tempo_voo << " |" << endl;
         }
     }
-    cout << "+---------+---------+------------------+---------------+" << endl << endl << endl;
+    cout << "+---------+---------+------------------+---------------+" << endl
+         << endl
+         << endl;
 }
 
-bool compareMensagens(const Mensagem& a, const Mensagem& b) {
-    if (a.voo.horario == b.voo.horario) {
-        if(a.voo.tempo_voo > b.voo.tempo_voo)
-             return a.voo.tempo_voo;
-        return  b.voo.horario;
+bool compareMensagens(const Mensagem &a, const Mensagem &b)
+{
+    if (a.voo.horario == b.voo.horario)
+        return a.voo.tempo_voo > b.voo.tempo_voo ? a.voo.tempo_voo : b.voo.horario;
+
+    return a.voo.horario;
+}
+
+vector<Voo> getVoos()
+{
+    vector<Voo> decolagens;
+    int quantidade;
+    cout << "Digite a quantidade de decolagens (-1 para decolagens predefinidas)" << endl;
+    cin >> quantidade;
+
+    if (quantidade < 0)
+        return {// codigo, origem, destino, horario, tempo_voo, is_pouso
+                {'1', 0, 1, 0, 3, false},
+                {'2', 1, 0, 0, 2, false},
+                {'3', 1, 0, 0, 3, false}};
+
+    for (int i = 0; i < quantidade; ++i)
+    {
+        cout << "Voo ( " << i+1 << " )" << endl;
+        Voo voo;
+        voo.is_pouso = false; // Sempre decolagem neste contexto
+        cout << "Digite o código do voo: ";
+        cin >> voo.codigo;
+        cout << "Digite origem do voo: ";
+        cin >> voo.origem;
+        cout << "Digite o destino do voo: ";
+        cin >> voo.destino;
+        cout << "Digite o horário do voo: ";
+        cin >> voo.horario;
+        cout << "Digite o tempo do voo: ";
+        cin >> voo.tempo_voo;
+
+        decolagens.push_back(voo);
     }
-    return  a.voo.horario;
+
+    return decolagens;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -73,51 +118,61 @@ int main(int argc, char** argv) {
 
     int lc = 0; // Relógio lógico
     vector<Voo> pousos;
-    vector<Voo> decolagens = { // codigo, origem, destino, horario, tempo_voo, is_pouso
-        {1, 0, 1, 0, 3, false},
-        {2, 1, 0, 0, 2, false},
-        {3, 1, 0, 0, 3, false}
-    };
+    vector<Voo> decolagens;
+
+    if(rank==0)
+        decolagens = getVoos();
+
+    int decolagens_size = decolagens.size();
+    MPI_Bcast(&decolagens_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    decolagens.resize(decolagens_size);
+    MPI_Bcast(decolagens.data(), decolagens_size * sizeof(Voo), MPI_BYTE, 0, MPI_COMM_WORLD);
+
     vector<Mensagem> mensagens;
 
-    // Formando códigos de voos 
+    // Formando códigos de voos
     int decolagem_count = 1;
     string resultado;
 
-    for (auto& voo : decolagens) {
-        if (voo.origem == rank) {
+    for (Voo &voo : decolagens)
+    {
+        if (voo.origem == rank)
+        {
             lc++;
-            resultado = to_string(((rank + 1) * 10) + decolagem_count); // Primeiro número é a origem, segundo é a quantidade de decolagens
-            voo.codigo = atoi(resultado.c_str());
+            //resultado = to_string(((rank + 1) * 10) + decolagem_count); // Primeiro número é a origem, segundo é a quantidade de decolagens
+            voo.codigo = to_string(rank + 1) + to_string(decolagem_count);
             decolagem_count++;
 
-            Mensagem msg = { lc, voo };
+            Mensagem msg = {lc, voo};
             mensagens.push_back(msg);
         }
     }
 
     // Enviar decolagens para os destinos
-    for (auto& msg : mensagens) {
-        if (msg.voo.origem == rank) {
+    for (Mensagem &msg : mensagens)
+    {
+        if (msg.voo.origem == rank)
             MPI_Send(&msg, sizeof(Mensagem), MPI_BYTE, msg.voo.destino, TAG, MPI_COMM_WORLD);
-        }
     }
 
     // Receber pousos de outros aeroportos
     int total_pousos_recebidos = 0;
     int total_pousos_esperados = 0;
-    for (const auto& voo : decolagens) {
-        if (voo.destino == rank) {
+    for (const Voo &voo : decolagens)
+    {
+        if (voo.destino == rank)
             total_pousos_esperados++;
-        }
     }
 
-    while (total_pousos_recebidos < total_pousos_esperados) {
+    while (total_pousos_recebidos < total_pousos_esperados)
+    {
         MPI_Status status;
         Mensagem msg;
         MPI_Recv(&msg, sizeof(Mensagem), MPI_BYTE, MPI_ANY_SOURCE, TAG, MPI_COMM_WORLD, &status);
 
-        if (msg.voo.destino == rank) {
+        if (msg.voo.destino == rank)
+        {
             msg.voo.is_pouso = true;
             pousos.push_back(msg.voo);
             mensagens.push_back(msg);
@@ -132,13 +187,13 @@ int main(int argc, char** argv) {
     vector<Mensagem> pista;
     int current_time = 0;
 
-    for (const auto& msg : mensagens) {
-        if (msg.voo.horario > current_time) {
+    for (const Mensagem &msg : mensagens)
+    {
+        if (msg.voo.horario > current_time)
             current_time = msg.voo.horario;
-        } else {
+        else
             current_time++;
-        }
-        pista.push_back({ current_time, msg.voo });
+        pista.push_back({current_time, msg.voo});
     }
 
     // Imprime a tabela de mensagens do aeroporto atual
